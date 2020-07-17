99eea61e15baf844fef7475cfef9b7c8a584b6f5
amor.edlouise <amor.edlouise@gmail.com>
2020-07-17 14:31:14 +0800

    Updates

---
+++ precache-manifest.2b42e60ac81c91a3ebd34b0edab01da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcc1d91a74d504d367a36991488296e",
+    "url": "/backapp3/index.html"
+  },
+  {
+    "revision": "bbaa98b581d3a9883e09",
+    "url": "/backapp3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9299057c4c9310b4edff",
+    "url": "/backapp3/static/js/2.6baaa894.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/backapp3/static/js/2.6baaa894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbaa98b581d3a9883e09",
+    "url": "/backapp3/static/js/main.dd7fe3b5.chunk.js"
+  },
+  {
+    "revision": "b9464f97fa26aaaa76c2",
+    "url": "/backapp3/static/js/runtime-main.5b932d06.js"
+  }
+]);
