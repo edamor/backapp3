631801870cef356e3ef1fae1abb6fa9aa1b3a83e
amor.edlouise <amor.edlouise@gmail.com>
2020-07-19 15:10:27 +0800

    Updates

--- precache-manifest.4acefc828d9859b7707607fa272b968e.js
+++ precache-manifest.4acefc828d9859b7707607fa272b968e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cab0ac4dc76ea81d7865e2ae444754c9",
+    "revision": "de34ac6a0461b51afa1927724f16c8ff",
     "url": "/backapp3/index.html"
   },
   {
-    "revision": "82f19f0cdd580589c137",
+    "revision": "1fc998f4ec9eeb345162",
     "url": "/backapp3/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "b812f96295b64741a6f9",
-    "url": "/backapp3/static/js/2.48f5388f.chunk.js"
+    "revision": "2669b474a06aa5c90d3a",
+    "url": "/backapp3/static/js/2.e0d8aae6.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/backapp3/static/js/2.48f5388f.chunk.js.LICENSE.txt"
+    "url": "/backapp3/static/js/2.e0d8aae6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "82f19f0cdd580589c137",
-    "url": "/backapp3/static/js/main.02a47c06.chunk.js"
+    "revision": "1fc998f4ec9eeb345162",
+    "url": "/backapp3/static/js/main.519e0353.chunk.js"
   },
   {
     "revision": "b9464f97fa26aaaa76c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/backapp3/precache-manifest.53d1f1c24f63b98318a281df22eb2cfd.js"
+  "/backapp3/precache-manifest.4acefc828d9859b7707607fa272b968e.js"
 );
 
 self.addEventListener('message', (event) => {
